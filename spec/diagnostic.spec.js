c52956f677c88b90e3fd66fd8afc613c8d0d468f
jcornmanhomonoff <jcornmanhomonoff@gmail.com>
2017-09-15 12:14:04 -0400

    Run `grunt make-standard`
    Make standardJS

--- spec/diagnostic.spec.js
+++ spec/diagnostic.spec.js
@@ -1,33 +1,32 @@
-'use strict';
+'use strict'
 
-const chai = require('chai');
-const _ = require('lodash');
+const chai = require('chai')
+const _ = require('lodash')
 
-const expect = chai.expect;
+const expect = chai.expect
 
-const diagnostic = require('../lib/diagnostic');
-
+const diagnostic = require('../lib/diagnostic')
 
 describe('answer1', function () {
   it('returns the correct value', () => {
-    expect(diagnostic.answer1).to.be.eql(NaN);
-  });
-});
+    expect(diagnostic.answer1).to.be.eql(NaN)
+  })
+})
 
 describe('answer2', () => {
   it('returns the correct value', () => {
-    expect(diagnostic.answer2).to.eql("Ben Adamski");
-  });
-});
+    expect(diagnostic.answer2).to.eql('Ben Adamski')
+  })
+})
 
 describe('answer3', () => {
   it('is called on the correct object', () => {
-    expect(diagnostic.answer3).to.eql('Santa Clause');
-  });
-});
+    expect(diagnostic.answer3).to.eql('Santa Clause')
+  })
+})
 
 describe('answer4', () => {
   it('is a function that can be called on the correct object', () => {
-    expect(diagnostic.answer4()).to.eql('Alderaan');
-  });
-});
+    expect(diagnostic.answer4()).to.eql('Alderaan')
+  })
+})
